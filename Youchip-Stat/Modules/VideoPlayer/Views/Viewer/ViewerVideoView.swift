89b5e72126eae587ecf7b7dcf879cff88497b7c8
Alan Erkenov <aer@aiweapps.com>
2026-03-18 12:49:45 +0300

    return viewer mode watermark padding

--- Youchip-Stat/Modules/VideoPlayer/Views/Viewer/ViewerVideoView.swift
+++ Youchip-Stat/Modules/VideoPlayer/Views/Viewer/ViewerVideoView.swift
@@ -144,7 +144,7 @@ struct ViewerVideoView: View {
                         }
                         .overlay(alignment: .bottom) {
                             videoOverlayText()
-                                .allowsHitTesting(false)
+                                .padding(.bottom, 45)
                         }
                 } else {
                     VStack(spacing: 12) {
